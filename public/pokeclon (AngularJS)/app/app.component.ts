cec2907d45c36a0cf99a1d5dbd4c6e4b9c261ec0
dafranze <dafranze@htwg-konstanz.de>
2016-12-21 03:43:32 +0100

    Fix

--- public/pokeclon (AngularJS)/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1><p class="text-warning">Hello {{name}}.. Here is Angular!!</p></h1>`,
-})
-export class AppComponent  { name = 'World';}
