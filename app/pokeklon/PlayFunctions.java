a40520d3938071247b6f8b7cd4386a295c854a85
Benedict Roth <Benedict Roth>
2017-01-18 12:19:04 +0100

    fix to try heroku

--- app/pokeklon/PlayFunctions.java
+++ app/pokeklon/PlayFunctions.java
@@ -241,12 +241,10 @@ public class PlayFunctions extends Controller{
 	{
 		Gson gson = new GsonBuilder().create();
 		WUIStatus state = new WUIStatus();
-		if(Pokeklon.controller != null && Pokeklon.controller.getGameStat() != null)
-			state.update = Pokeklon.controller.getGameStat();
-		if(Pokeklon.tui != null && Pokeklon.tui.output != null)
-			state.tui = Pokeklon.tui.output.replace("\n", "<br>");
 		if(Pokeklon.controller != null)
 		{
+			state.update = Pokeklon.controller.getGameStat();
+			state.tui = Pokeklon.tui.output.replace("\n", "<br>");
 			state.maxMo = Pokeklon.controller.getNoOfMonster();
 			if(Pokeklon.controller.getPlayer1() != null && Pokeklon.controller.getPlayer2() != null)
 			{
